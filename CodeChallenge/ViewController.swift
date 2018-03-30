2dc8d45bd65f674fd18dfb41db096f52b8fd3dbb
Chris Manahan <cmanahan@grubhub.com>
2018-03-30 13:44:22 -0400

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by cmanahan on 3/30/18.
+//  Copyright © 2018 Chris Manahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
